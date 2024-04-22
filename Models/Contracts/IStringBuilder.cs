ca8ec21cd2f8554bc738cafe978b4162b310d67b
Denis <125385668+denjiyy@users.noreply.github.com>
2024-04-22 18:54:28 +0300

    Add files via upload

---
+++ Models/Contracts/IStringBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTML.Models.Contracts
+{
+    public interface IStringBuilder
+    {
+        IStringBuilder Append(string value);
+        IStringBuilder Clear();
+        int Length { get; }
+        int Capacity { get; }
+    }
+}
