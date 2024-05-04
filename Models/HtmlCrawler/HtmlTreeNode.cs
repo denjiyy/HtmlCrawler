4679ca93beafc05fd5a19dd699c65e1535ed0cca
Denis <125385668+denjiyy@users.noreply.github.com>
2024-05-04 13:59:19 +0300

    Add files via upload

---
+++ Models/HtmlCrawler/HtmlTreeNode.cs
@@ -0,0 +1,22 @@
+﻿using CrawlerHTML.CustomDataStructures;
+
+namespace HtmlCrawl
+{
+    public class HtmlTreeNode
+    {
+        public string Tag { get; }
+        public CustomList<HtmlTreeNode> Children { get; }
+        public string Content { get; private set; }
+        public CustomDictionary<string, string> Attributes { get; private set; }
+
+        //public HtmlTreeNode Parent { get; set; } vse taq
+
+        public HtmlTreeNode(string tag, string content)
+        {
+            Tag = tag;
+            Children = new CustomList<HtmlTreeNode>();
+            Content = content;
+            Attributes = new CustomDictionary<string, string>();
+        }
+    }
+}
