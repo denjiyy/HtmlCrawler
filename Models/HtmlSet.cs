1df665a5e18e43d3e85be7e7c8a4a4a51faeec33
Denis <125385668+denjiyy@users.noreply.github.com>
2024-05-04 13:58:03 +0300

    Add files via upload

---
+++ Models/HtmlSet.cs
@@ -0,0 +1,24 @@
+﻿using HtmlCrawl.Contracts;
+
+namespace HtmlCrawl.HtmlCrawler
+{
+    public class HtmlSet : IHtmlOperation
+    {
+        private readonly HtmlSearch htmlSearch;
+
+        public HtmlSet(HtmlSearch htmlSearch)
+        {
+            this.htmlSearch = htmlSearch;
+        }
+
+        public void PerformOperation(string relativePath, string newContent)
+        {
+            var nodesToSet = htmlSearch.Find(relativePath);
+
+            foreach (var node in nodesToSet)
+            {
+                node.Content = newContent;
+            }
+        }
+    }
+}
