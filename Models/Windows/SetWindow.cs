88e817d2df6fb79b8c5b2cf9f5a11c53a8ad14ad
Denis <125385668+denjiyy@users.noreply.github.com>
2024-05-16 16:24:08 +0300

    Add files via upload

---
+++ Models/Windows/SetWindow.cs
@@ -0,0 +1,45 @@
+﻿using CrawlerHTML;
+using HtmlCrawl;
+using HtmlCrawl.HtmlCrawler;
+
+namespace HTML
+{
+    public partial class SetWindow : Form
+    {
+        private HtmlTreeNode root;
+        private HtmlSearch search;
+        private HtmlSet set;
+        public SetWindow(HtmlTreeNode root)
+        {
+            InitializeComponent();
+
+            this.root = root;
+
+            search = new HtmlSearch(root);
+            set = new HtmlSet(search);
+        }
+
+        private void BackLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            HtmlCrawlerWindow htmlCrawlerWindow = new HtmlCrawlerWindow(root);
+            this.Hide();
+            htmlCrawlerWindow.Show();
+        }
+
+        private void SetRelativePath_Click(object sender, EventArgs e)
+        {
+            string relativePath = RelativePath.Text;
+            string newContent = NewContent.Text;
+
+            if (CustomLinqExtensions.CustomIsNullOrEmpty(newContent))
+            {
+                MessageBox.Show("Please enter a value for the new content.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            set.PerformOperation(relativePath, newContent);
+
+            MessageBox.Show("Set operation completed.", "Operation Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
