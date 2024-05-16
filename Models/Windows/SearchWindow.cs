75bd77bd535859951e8ccaa37755fd9beebe937e
Denis <125385668+denjiyy@users.noreply.github.com>
2024-05-16 16:23:04 +0300

    Add files via upload

---
+++ Models/Windows/SearchWindow.cs
@@ -0,0 +1,36 @@
+﻿using CrawlerHTML.CustomDataStructures;
+using HTML.Models.Windows;
+using HtmlCrawl;
+using HtmlCrawl.HtmlCrawler;
+
+namespace HTML
+{
+    public partial class SearchWindow : Form
+    {
+        private HtmlTreeNode root;
+        private HtmlSearch search;
+
+        public SearchWindow(HtmlTreeNode root)
+        {
+            InitializeComponent();
+            this.root = root;
+
+            search = new HtmlSearch(root);
+        }
+
+        private void BackLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void SearchRelativePath_Click(object sender, EventArgs e)
+        {
+            string relativePath = RelativePathTextBox.Text;
+            CustomList<HtmlTreeNode> searchResults = search.Find(relativePath);
+
+            SearchResultWindow searchResultWindow = new SearchResultWindow(root, searchResults, RelativePathTextBox.Text);
+            this.Hide();
+            searchResultWindow.Show();
+        }
+    }
+}
