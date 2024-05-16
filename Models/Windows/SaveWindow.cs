8ef0e946e0c1168c6707069fe4de0a96aef3e1ac
Denis <125385668+denjiyy@users.noreply.github.com>
2024-05-16 16:20:44 +0300

    Add files via upload

---
+++ Models/Windows/SaveWindow.cs
@@ -0,0 +1,48 @@
+﻿using CrawlerHTML;
+using HtmlCrawl;
+
+namespace HTML.Models.Windows
+{
+    public partial class SaveWindow : Form
+    {
+        private HtmlTreeNode root;
+        private HtmlSaver saver;
+
+        public SaveWindow(HtmlTreeNode root)
+        {
+            InitializeComponent();
+
+            this.root = root;
+            saver = new HtmlSaver(root);
+        }
+
+        private void BackLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void SaveDocument_Click(object sender, EventArgs e)
+        {
+            string filePath = RelativePathTextBox.Text;
+
+            if (CustomLinqExtensions.CustomIsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("Please enter a valid file path.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                saver.SaveHtmlFile(filePath);
+
+                MessageBox.Show("HTML file saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving HTML file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            this.Hide();
+        }
+    }
+}
